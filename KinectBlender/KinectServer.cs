3304b12dd5476854662a286a8293f896b25accc2
Pete Dring <dringp@fulford.york.sch.uk>
2024-04-15 14:10:17 +0100

    Add project files.

---
+++ KinectBlender/KinectServer.cs
@@ -0,0 +1,15 @@
+namespace KinectBlender
+{
+    public partial class KinectServer : Form
+    {
+        public KinectServer()
+        {
+            InitializeComponent();
+        }
+
+        private void KinectServer_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
