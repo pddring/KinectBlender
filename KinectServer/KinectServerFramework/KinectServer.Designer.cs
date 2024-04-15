4425218d68eb9a8b79eb60fc203c49450852cbf7
Pete Dring <dringp@fulford.york.sch.uk>
2024-04-15 21:00:38 +0100

    Added icon and detected kinect sensor

---
+++ KinectServer/KinectServerFramework/KinectServer.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace KinectServerFramework
+{
+    partial class KinectServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KinectServer));
+            this.grpStatus = new System.Windows.Forms.GroupBox();
+            this.panelStatus = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.grpLog = new System.Windows.Forms.GroupBox();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.grpStatus.SuspendLayout();
+            this.panelStatus.SuspendLayout();
+            this.grpLog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpStatus
+            // 
+            this.grpStatus.Controls.Add(this.panelStatus);
+            this.grpStatus.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grpStatus.Location = new System.Drawing.Point(0, 0);
+            this.grpStatus.Name = "grpStatus";
+            this.grpStatus.Size = new System.Drawing.Size(800, 100);
+            this.grpStatus.TabIndex = 0;
+            this.grpStatus.TabStop = false;
+            this.grpStatus.Text = "Status:";
+            // 
+            // panelStatus
+            // 
+            this.panelStatus.Controls.Add(this.lblStatus);
+            this.panelStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelStatus.Location = new System.Drawing.Point(3, 16);
+            this.panelStatus.Name = "panelStatus";
+            this.panelStatus.Size = new System.Drawing.Size(794, 81);
+            this.panelStatus.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(3, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "label1";
+            // 
+            // grpLog
+            // 
+            this.grpLog.Controls.Add(this.lstLog);
+            this.grpLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpLog.Location = new System.Drawing.Point(0, 100);
+            this.grpLog.Name = "grpLog";
+            this.grpLog.Size = new System.Drawing.Size(800, 350);
+            this.grpLog.TabIndex = 0;
+            this.grpLog.TabStop = false;
+            this.grpLog.Text = "Log:";
+            // 
+            // lstLog
+            // 
+            this.lstLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(3, 16);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(794, 331);
+            this.lstLog.TabIndex = 0;
+            // 
+            // KinectServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grpLog);
+            this.Controls.Add(this.grpStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "KinectServer";
+            this.Text = "Kinect Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpStatus.ResumeLayout(false);
+            this.panelStatus.ResumeLayout(false);
+            this.panelStatus.PerformLayout();
+            this.grpLog.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpStatus;
+        private System.Windows.Forms.FlowLayoutPanel panelStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.GroupBox grpLog;
+        private System.Windows.Forms.ListBox lstLog;
+    }
+}
+
---
+++ KinectServer/KinectServerFramework/KinectServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+
+namespace KinectServerFramework
+{
+    public partial class KinectServer : Form
+    {
+        public KinectServer()
+        {
+            InitializeComponent();
+        }
+        
+        KinectSensor k = KinectSensor.GetDefault();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            k.IsAvailableChanged += K_IsAvailableChanged;
+            if (k.IsAvailable)
+            {
+                k.Open();
+            }
+            UpdateStatus();
+        }
+
+        private void K_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            UpdateStatus();
+        }
+
+        private void UpdateStatus()
+        {
+            lblStatus.Text = $"Available: {k.IsAvailable} Connected: {k.IsOpen}";
+        }
+    }
+}
